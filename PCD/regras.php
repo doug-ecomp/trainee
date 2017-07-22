64203af6b552b37a96158a4c1eb69be6c526cdf6
Douglas Carneiro <douglas.ecomp12@gmail.com>
2017-07-22 02:20:46 -0300

    Fixing conflicts from mistaken merge

--- PCD/regras.php
+++ PCD/regras.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -22,20 +21,6 @@
             include("footer.html");
         ?>
    
-=======
-</head>
-<body>
-    <header>
-        <?php
-            include("header.html");
-        ?>
-    </header>
-    <footer>
-        <?php
-            include("footer.html");
-        ?>
-    </footer>
->>>>>>> d698bd0a2c10a69a257082a68f759fa3f0618fad
     
 </body>
 </html>
